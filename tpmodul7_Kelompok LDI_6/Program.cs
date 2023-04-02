b93c2b9b5cb209a59dbb0ba935b7153794a26afe
Yakup-TBR <yakupasmaidyatanggae@gmail.com>
2023-04-02 23:39:37 +0800

    “menambahkan class KuliahMahasiswa_1302210113

--- tpmodul7_Kelompok LDI_6/Program.cs
+++ tpmodul7_Kelompok LDI_6/Program.cs
@@ -3,5 +3,8 @@ using tpmodul7_Kelompok_LDI_6;
 
 
 DataMahasiswa1302210113_PRAKTIKAN objMhs = new DataMahasiswa1302210113_PRAKTIKAN();
+KuliahMahasiswa_1302210113_PRAKTIKAN objklh = new KuliahMahasiswa_1302210113_PRAKTIKAN();
+
 
 objMhs.readJSON();
+objklh.readJSON();
