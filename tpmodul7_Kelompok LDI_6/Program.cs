07e7d9b2cc25dd06bb93d7df7393552a9216b79f
mghazyfh <127060793+mghazyfh@users.noreply.github.com>
2023-04-02 22:34:37 +0700

    commit 2

--- tpmodul7_Kelompok LDI_6/Program.cs
+++ tpmodul7_Kelompok LDI_6/Program.cs
@@ -1,5 +1,7 @@
 ﻿using tpmodul7_Kelompok_LDI_6;
 
 DataMahasiswa1302210027 objMhs = new DataMahasiswa1302210027();
+KuliahMahasiswa1302210027 objKlh = new KuliahMahasiswa1302210027();
 
 objMhs.readJSON();
+objKlh.readJSON();
